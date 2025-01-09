116d073fb9ad5eecd0dc2a42f0ce4b71c60a6f77
Joao-Oliveira9 <joaoliveira9@outlook.com>
2025-01-09 16:24:09 -0300

    verificando as entradas do twitter

--- attenpt2.py
+++ attenpt2.py
@@ -2,14 +2,18 @@ import pyshark
 import asyncio
 
 capture = pyshark.LiveCapture(interface="Ethernet",bpf_filter="port 53")
+count = 0
 
 def print_packets(packet) -> None:
     try:
-        print(packet)
+        print(packet.dns.qry_name)
+        if packet.dns.qry_name == "x.com":
+             count = count + 1
     except asyncio.TimeoutError:
         pass
 
 try:
     capture.apply_on_packets(print_packets,timeout=5)
+    print(count)
 except asyncio.TimeoutError:
         pass
